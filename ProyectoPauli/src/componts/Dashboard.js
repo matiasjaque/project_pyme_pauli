f9424d58741ab9d6fc1473fc40853d73a7a20eed
Isaac Jaque Montecinos <aron.jaque94@gmail.com>
2023-06-20 22:25:23 -0400

    Dashboard
    Se mejora la estructura del carrusel

--- ProyectoPauli/src/componts/Dashboard.js
+++ ProyectoPauli/src/componts/Dashboard.js
@@ -2,51 +2,41 @@ import React from "react";
 import "../styles/Dashboard.css";
 import Carousel from "react-bootstrap/Carousel";
 
-import logo2 from '../assets/logos/logo2.png';
-import logo3 from '../assets/logos/logo3.png';
-import logo4 from '../assets/logos/logo4.png';
+import logo2 from "../assets/logos/logo2.png";
+import logo3 from "../assets/logos/logo3.png";
+import logo4 from "../assets/logos/logo4.png";
 
 const Dashboard = () => {
+  // arreglo de productos estaticos
+
+  const imagenesCarusel = [
+    {
+      imgLogo: logo2,
+      nombre: "logo 2",
+    },
+    {
+      imgLogo: logo3,
+      nombre: "logo 3",
+    },
+    {
+      imgLogo: logo4,
+      nombre: "logo 4",
+    },
+    {
+      imgLogo:
+        "https://www.shutterstock.com/image-vector/sweet-cake-logo-template-bakery-260nw-2182238775.jpg",
+      nombre: "torta",
+    },
+  ];
+
   return (
     <div id="dashboard">
       <Carousel id="contenedorCarousel">
-        <Carousel.Item className="contenedorSecundarioCarousel">
-          <img
-            className="d-block w-100"
-            src={logo2}
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item className="contenedorSecundarioCarousel">
-          <img
-            className="d-block w-100"
-            src={logo3}
-            alt="Second slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item className="contenedorSecundarioCarousel">
-          <img
-            className="d-block w-100"
-            src={logo4}
-            alt="Third slide"
-          /> 
-
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
+        {imagenesCarusel.map((imagenCarusel) => (
+          <Carousel.Item className="contenedorSecundarioCarousel">
+            <img src={imagenCarusel.imgLogo} alt={imagenCarusel.nombre} />
+          </Carousel.Item>
+        ))}
       </Carousel>
     </div>
   );
